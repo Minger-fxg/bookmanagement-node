d857649994fe1956374917125d825eca636c3126
Minger <16773727176@qq.com>
2021-05-11 09:34:57 +0800

    新增注册

--- routes/register.js
+++ routes/register.js
@@ -3,13 +3,19 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-    console.log(req.query);
-    res.send('zhuce1');
+    let inform = req.query;
+
+    let username = inform.newUsername;
+    let password = inform.newPassword;
+    let result = {
+        state: 'success',
+        message: "注册成功!!!您的账号为" + username
+    };
+    res.send(result);
 });
 
 router.post('/', function(req, res, next) {
-    console.log(req.query);
-    res.send('zhuce2');
+
 });
 
 module.exports = router;
